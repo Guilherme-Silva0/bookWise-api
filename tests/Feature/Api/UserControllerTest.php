7c008a6d653adf2852af5b59020e7b026d80479a
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-12-29 21:28:19 -0300

    test: :white_check_mark: Test validate user registration

--- tests/Feature/Api/UserControllerTest.php
+++ tests/Feature/Api/UserControllerTest.php
@@ -16,11 +16,12 @@ class UserControllerTest extends TestCase
     {
         $user = User::factory()->makeOne();
 
-        $response = $this->postJson('/api/user/register', [
+        $response = $this->postJson('/api/user/register?lang=pt_BR', [
             'first_name' => $user->first_name,
             'last_name' => $user->last_name,
             'email' => $user->email,
             'password' => 'password',
+            'password_confirmation' => 'password',
         ]);
 
         $response->assertStatus(201);
@@ -40,6 +41,57 @@ class UserControllerTest extends TestCase
                 'last_name' => $user->last_name,
                 'email' => $user->email,
             ])->etc();
+        });
+    }
+
+    public function test_validate_user_registration_empty_fields(): void
+    {
+        $response = $this->postJson('/api/user/register?lang=pt_BR', [
+            'first_name' => '',
+            'last_name' => '',
+            'email' => '',
+            'password' => '',
+            'password_confirmation' => '',
+        ]);
+
+        $response->assertStatus(422);
+
+        $response->assertJson(function (AssertableJson $json) {
+            $json->hasAll(['message', 'errors']);
+
+            $json->whereAll([
+                'message' => 'O campo primeiro nome é obrigatório. (e mais 3 erros)',
+                'errors.first_name.0' => 'O campo primeiro nome é obrigatório.',
+                'errors.last_name.0' => 'O campo sobrenome é obrigatório.',
+                'errors.email.0' => 'O campo email é obrigatório.',
+                'errors.password.0' => 'O campo senha é obrigatório.',
+            ]);
+        });
+    }
+
+    public function test_validate_user_registration_invalid_fields(): void
+    {
+        $response = $this->postJson('/api/user/register?lang=pt_BR', [
+            'first_name' => 'J',
+            'last_name' => 'D',
+            'email' => 'j',
+            'password' => 'p',
+            'password_confirmation' => 'j',
+        ]);
+    
+        $response->assertStatus(422);
+
+        $response->assertJson(function (AssertableJson $json) {
+            $json->hasAll(['message', 'errors']);
+
+            $json->whereAll([
+                'message' => 'O campo primeiro nome deve ter pelo menos 2 caracteres. (e mais 4 erros)',
+                'errors.first_name.0' => 'O campo primeiro nome deve ter pelo menos 2 caracteres.',
+                'errors.last_name.0' => 'O campo sobrenome deve ter pelo menos 2 caracteres.',
+                'errors.email.0' => 'O campo email deve ser um endereço de e-mail válido.',
+                'errors.password.0' => 'O campo senha deve ter pelo menos 8 caracteres.',
+                'errors.password.1' => 'O campo senha de confirmação não confere.',
+            ]);
         });
     }
 }
