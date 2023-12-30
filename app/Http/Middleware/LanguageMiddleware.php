5e6d2880a65dbfea96d493a878ac35574f8108ac
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-12-29 21:24:47 -0300

    feat: :sparkles: Create middleware to select language

---
+++ app/Http/Middleware/LanguageMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class LanguageMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $lang = $request->get('lang');
+
+        if ($lang && in_array($lang, ['pt_BR', 'en'])) {
+            app()->setLocale($lang);
+        }
+
+        return $next($request);
+    }
+}
