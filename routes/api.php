2a2da7bf72d60abe4e40cdadc61b8e0926c58a1c
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-12-30 20:12:13 -0300

    feat: :sparkles: Create a middleware for auth and add route user.me

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('user/register', [UserController::class, 'store'])->name('user.store');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [UserController::class, 'me'])->name('user.me');
+});
