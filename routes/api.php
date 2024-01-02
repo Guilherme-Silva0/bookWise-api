ea3068ad9cd8d13337bc599888e68b70e2acf3af
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2024-01-01 21:32:07 -0300

    feat: :sparkles: Add user login route

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('user/register', [UserController::class, 'store'])->name('user.store');
+Route::post('user/login', [UserController::class, 'login'])->name('user.login');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [UserController::class, 'me'])->name('user.me');
