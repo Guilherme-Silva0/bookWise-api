0b39e89c6406a4394b632c6e26a53c17c0733859
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2024-01-03 20:07:16 -0300

    feat: :sparkles: Create update user validation

---
+++ app/Http/Requests/Api/UserUpdateRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()->id === $this->route('id');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => ['sometimes','string','min:2' ,'max:255'],
+            'last_name' => ['sometimes','string','min:2' , 'max:255'],
+            'email' => ['sometimes','string', 'email', 'max:255', 'unique:users,email'],
+            'profile_image' => ['sometimes','string', 'max:255'],
+            'profile_info' => ['sometimes','string', 'min:2' ,'max:255'],
+            'status' => ['sometimes','string', 'in:active,inactive,banned'],
+            'user_type' => ['sometimes','string', 'in:normal,writer'],
+        ];
+    }
+}
