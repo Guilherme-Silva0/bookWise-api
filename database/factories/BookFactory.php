dd3affb1cb576122d2c1845323e7c9697db90740
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-01-15 16:34:57 -0300

    feat: :sparkles: Create book factory

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->title(),
+            'description' => $this->faker->text(),
+            'price' => $this->faker->randomFloat(2, 0, 100),
+            'condition' => $this->faker->randomElement(['new', 'used']),
+            'genre' => $this->faker->word(),
+            'isbn' => $this->faker->isbn13(),
+            'publication_year' => $this->faker->year(),
+            'language' => $this->faker->languageCode(),
+            'page_count' => $this->faker->numberBetween(1, 1000),
+            'publisher' => $this->faker->company(),
+            'added_date' => $this->faker->date(),
+            'image_path' => $this->faker->imageUrl(),
+            'availability' => $this->faker->boolean(),
+        ];
+    }
+}
