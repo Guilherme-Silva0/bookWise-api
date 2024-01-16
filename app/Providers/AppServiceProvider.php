0b4901a03ce3a0ccc9655a4dd976893874658ff1
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-01-16 19:18:11 -0300

    feat: :sparkles: Bind book repository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Repositories\Contracts\BookRepositoryInterface;
 use App\Repositories\Contracts\UserRepositoryInterface;
+use App\Repositories\Eloquent\BookRepository;
 use App\Repositories\Eloquent\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
@@ -16,6 +18,11 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(
             UserRepositoryInterface::class,
             UserRepository::class
+        );
+
+        $this->app->bind(
+            BookRepositoryInterface::class,
+            BookRepository::class
         );
     }
 
