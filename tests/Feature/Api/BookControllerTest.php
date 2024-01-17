297e56744b8a6afa83f3ebca8eedebe6a0608e58
Guilherme-Silva0 <guilhermesilvadev0@gmail.com>
2024-01-17 14:45:02 -0300

    test: :test_tube: Test can show book

--- tests/Feature/Api/BookControllerTest.php
+++ tests/Feature/Api/BookControllerTest.php
@@ -44,4 +44,55 @@ class BookControllerTest extends TestCase
             ]
         ]);
     }
+
+    public function test_can_show_book(): void
+    {
+        $book = Book::factory()->create();
+
+        $response = $this->getJson("/api/books/{$book->id}");
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'data' => [
+                'id',
+                'title',
+                'description',
+                'price',
+                'condition',
+                'genre',
+                'isbn',
+                'publication_year',
+                'language',
+                'page_count',
+                'publisher',
+                'added_date',
+                'image_path',
+                'availability',
+                'created_at',
+                'updated_at',
+            ]
+        ]);
+
+        $response->assertJson([
+            'data' => [
+                'id' => $book->id,
+                'title' => $book->title,
+                'description' => $book->description,
+                'price' => $book->price,
+                'condition' => $book->condition,
+                'genre' => $book->genre,
+                'isbn' => $book->isbn,
+                'publication_year' => $book->publication_year,
+                'language' => $book->language,
+                'page_count' => $book->page_count,
+                'publisher' => $book->publisher,
+                'added_date' => $book->added_date,
+                'image_path' => $book->image_path,
+                'availability' => $book->availability,
+                'created_at' => $book->created_at->toJSON(),
+                'updated_at' => $book->updated_at->toJSON(),
+            ]
+        ]);
+    }
 }
